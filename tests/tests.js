8a9446343fe63c561e1829a84eec65f70de99085
Borja <alu0100498820@ull.edu.es>
2015-10-29 15:38:12 +0000

    modificando

--- tests/tests.js
+++ tests/tests.js
@@ -58,23 +58,18 @@ describe("Test para ConverTemp", function() {
 
 
     it("45C = 113F", function() {
-      var temp = new Temperatura();
-
-      temp.set_valor(45);
-      temp.set_tipo("C");
-
-      var result =  temp.pasar_a_f();
-  
+      
+      original.value = "45C";
 
       var espia = sinon.spy();
-      espia(result);
+      espia(convertir());
 
       expect(espia.called).to.be.true;
       expect(espia.calledOnce).to.be.true;
       expect(espia.firstCall.calledWith("113 Fahrenheit")).to.be.true;
       expect(espia.firstCall.calledWith(sinon.match.string)).to.be.true;
 
-      expect(result).to.equal("113 Fahrenheit");
+      expect(converted.innerHTML).to.equal("113 Fahrenheit");
     });
 
     it("convertir()", function() {
